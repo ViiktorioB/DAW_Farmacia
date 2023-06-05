d810ed09409515398e8a9c7cd0c72ca4245a42ff
ViiktorioB <115166390+ViiktorioB@users.noreply.github.com>
2023-06-05 15:25:54 +0200

    Web-Project

---
+++ Tomcat1/src/main/java/Login1.java
@@ -0,0 +1,38 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class Login1
+ */
+public class Login1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String mail = request.getParameter("mail");
+		String pass = request.getParameter("pass");
+		
+		System.out.println(mail);
+		System.out.println(pass);
+		
+		response.getWriter().append("ok");
+	}
+	
+
+}
